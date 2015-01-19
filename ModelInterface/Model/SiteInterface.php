d28db5a8869bd2b3876ae07060ee8702c569f255
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-01-19 12:20:23 +0100

    add meta

--- ModelInterface/Model/SiteInterface.php
+++ ModelInterface/Model/SiteInterface.php
@@ -46,6 +46,46 @@ interface SiteInterface
     public function getDefaultLanguage();
 
     /**
+     * @param string $metaKeywords
+     */
+    public function setMetaKeywords($metaKeywords);
+
+    /**
+     * @return string
+     */
+    public function getMetaKeywords();
+
+    /**
+     * @param string $metaDescription
+     */
+    public function setMetaDescription($metaDescription);
+
+    /**
+     * @return string
+     */
+    public function getMetaDescription();
+
+    /**
+     * @param boolean $metaIndex
+     */
+    public function setMetaIndex($metaIndex);
+
+    /**
+     * @return boolean
+     */
+    public function getMetaIndex();
+
+    /**
+     * @param boolean $metaFollow
+     */
+    public function setMetaFollow($metaFollow);
+
+    /**
+     * @return boolean
+     */
+    public function getMetaFollow();
+
+    /**
      * @param string $domain
      */
     public function setDomain($domain);
