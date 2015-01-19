63bc68daf58f9431e3a6a4c02f8c3672351b0a83
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-01-19 14:03:29 +0100

    add meta

--- ModelInterface/Model/NodeInterface.php
+++ ModelInterface/Model/NodeInterface.php
@@ -149,6 +149,46 @@ interface NodeInterface extends AreaContainerInterface, BlockContainerInterface,
     public function getLanguage();
 
     /**
+     * @param string $metaKeywords
+     */
+    public function setMetaKeywords($metaKeywords);
+
+    /**
+     * @return string
+    */
+    public function getMetaKeywords();
+
+    /**
+     * @param string $metaDescription
+    */
+    public function setMetaDescription($metaDescription);
+
+    /**
+     * @return string
+    */
+    public function getMetaDescription();
+
+    /**
+     * @param boolean $metaIndex
+    */
+    public function setMetaIndex($metaIndex);
+
+    /**
+     * @return boolean
+    */
+    public function getMetaIndex();
+
+    /**
+     * @param boolean $metaFollow
+    */
+    public function setMetaFollow($metaFollow);
+
+    /**
+     * @return boolean
+    */
+    public function getMetaFollow();
+
+    /**
      * Set deleted
      *
      * @param boolean $deleted
