7369336d6d66f0ef000baf6e488a80197c53e1b7
Nicolas Thal <nico.th4l@gmail.com>
2015-02-19 11:02:22 +0100

    add default scheme in interface not in form type

--- ModelInterface/Model/SchemeAbilityInterface.php
+++ ModelInterface/Model/SchemeAbilityInterface.php
@@ -7,6 +7,7 @@ namespace PHPOrchestra\ModelInterface\Model;
  */
 interface SchemeAbilityInterface
 {
+    const SCHEME_DEFAULT = 'default';
     const SCHEME_HTTP = 'http';
     const SCHEME_HTTPS = 'https';
     const SCHEME_FILE = 'file';
