77834eca5684861812dcde18820f9c87cd4a41d7
Nicolas Thal <nico.th4l@gmail.com>
2015-03-25 10:50:39 +0100

    split redirection interface

---
+++ ModelInterface/Model/ReadRedirectionInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OpenOrchestra\ModelInterface\Model;
+
+/**
+ * Interface ReadRedirectionInterface
+ */
+interface ReadRedirectionInterface
+{
+    /**
+     * @return string
+     */
+    public function getId();
+
+    /**
+     * @return string
+     */
+    public function getSiteId();
+
+    /**
+     * @return string
+     */
+    public function getLocale();
+
+    /**
+     * @return string
+     */
+    public function getSiteName();
+
+    /**
+     * @return string
+     */
+    public function getRoutePattern();
+
+    /**
+     * @return string
+     */
+    public function getNodeId();
+
+    /**
+     * @return string
+     */
+    public function getUrl();
+
+    /**
+     * @return boolean
+     */
+    public function isPermanent();
+}
--- ModelInterface/Model/RedirectionInterface.php
+++ ModelInterface/Model/RedirectionInterface.php
@@ -5,22 +5,12 @@ namespace OpenOrchestra\ModelInterface\Model;
 /**
  * Interface RedirectionInterface
  */
-interface RedirectionInterface
+interface RedirectionInterface extends ReadRedirectionInterface
 {
-    /**
-     * @return string
-     */
-    public function getId();
-
     /**
      * @param string $siteId
      */
     public function setSiteId($siteId);
-
-    /**
-     * @return string
-     */
-    public function getSiteId();
 
     /**
      * @param string $locale
@@ -28,19 +18,9 @@ interface RedirectionInterface
     public function setLocale($locale);
 
     /**
-     * @return string
-     */
-    public function getLocale();
-
-    /**
      * @param string $siteName
      */
     public function setSiteName($siteName);
-
-    /**
-     * @return string
-     */
-    public function getSiteName();
 
     /**
      * @param string $routePattern
@@ -48,34 +28,14 @@ interface RedirectionInterface
     public function setRoutePattern($routePattern);
 
     /**
-     * @return string
-     */
-    public function getRoutePattern();
-
-    /**
      * @param string $nodeId
      */
     public function setNodeId($nodeId);
 
     /**
-     * @return string
-     */
-    public function getNodeId();
-
-    /**
      * @param string $url
      */
     public function setUrl($url);
-
-    /**
-     * @return string
-     */
-    public function getUrl();
-
-    /**
-     * @return boolean
-     */
-    public function isPermanent();
 
     /**
      * @param boolean $permanent
