7ca3771dd4985801e8a410b40bc360abf4089a64
romain boessel <romain.boessel@businessecision.com>
2015-06-29 15:24:01 +0200

    fix comment

--- ModelInterface/Repository/NodeRepositoryInterface.php
+++ ModelInterface/Repository/NodeRepositoryInterface.php
@@ -100,7 +100,7 @@ interface NodeRepositoryInterface extends ReadNodeRepositoryInterface
      * @param string $siteId
      * @param string $type
      *
-     * @deprecated will be removed in 0.3.0, use findNotDeletedInLastVersionBySiteId instead
+     * @deprecated will be removed in 0.3.0, use findDeletedInLastVersionBySiteId instead
      *
      * @return array
      */
