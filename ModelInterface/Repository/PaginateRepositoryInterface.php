064220f4d62d3c8899100e5ff2c141adc200f4eb
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-07-07 17:51:35 +0200

    update dataTable parameter

--- ModelInterface/Repository/PaginateRepositoryInterface.php
+++ ModelInterface/Repository/PaginateRepositoryInterface.php
@@ -11,36 +11,11 @@ use OpenOrchestra\Pagination\Configuration\FinderConfiguration;
 interface PaginateRepositoryInterface
 {
     /**
-     * @param array|null  $descriptionEntity
-     * @param array|null  $columns
-     * @param string|null $search
-     * @param array|null  $order
-     * @param int|null    $skip
-     * @param int|null    $limit
-     *
-     * @deprecated will be removed in 0.3.0, use findForPaginate instead
-     *
-     * @return array
-     */
-    public function findForPaginateAndSearch($descriptionEntity = null, $columns = null, $search = null, $order = null, $skip = null, $limit = null);
-
-    /**
      * @param PaginateFinderConfiguration $configuration
      *
      * @return array
      */
     public function findForPaginate(PaginateFinderConfiguration $configuration);
-
-    /**
-     * @param string|null $descriptionEntity
-     * @param string|null $columns
-     * @param string|null $search
-     *
-     * @deprecated will be removed in 0.3.0, use countWithFilter instead
-     *
-     * @return mixed
-     */
-    public function countWithSearchFilter($descriptionEntity = null, $columns = null, $search = null);
 
     /**
      * @param FinderConfiguration $configuration
