9c4192d22fb982382874ca45b396960c516b9675
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-07-20 12:37:57 +0200

    delete dependency from mongo in controller

---
+++ ModelInterface/Manager/NodeManagerInterface.php
@@ -0,0 +1,26 @@
+<?php
+namespace OpenOrchestra\ModelInterface\Manager;
+
+
+/**
+ * Class NodeManagerInterface
+ */
+interface NodeManagerInterface
+{
+    /**
+     * @param string $language
+     * @param string $siteId
+     *
+     * @return NodeInterface
+     */
+    public function createTransverseNode($language, $siteId);
+
+    /**
+     * @param string $nodeId
+     * @param string $siteId
+     * @param string $language
+     *
+     * @return NodeInterface
+     */
+    public function duplicateNode($nodeId, $siteId, $language);
+}
