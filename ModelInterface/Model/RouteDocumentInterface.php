5dd4b45431ee58ba099b2bbf92f30e6ad348ec8f
Nicolas Thal <nico.th4l@gmail.com>
2015-09-24 17:30:45 +0200

    complete interface

--- ModelInterface/Model/RouteDocumentInterface.php
+++ ModelInterface/Model/RouteDocumentInterface.php
@@ -13,14 +13,29 @@ interface RouteDocumentInterface
     public function getName();
 
     /**
+     * @param string $name
+     */
+    public function setName($name);
+
+    /**
      * @return string
      */
     public function getPattern();
 
     /**
+     * @param string $pattern
+     */
+    public function setPattern($pattern);
+
+    /**
      * @return array
      */
     public function getDefaults();
+
+    /**
+     * @param array $defaults
+     */
+    public function setDefaults(array $defaults);
 
     /**
      * @return array
@@ -38,9 +53,19 @@ interface RouteDocumentInterface
     public function getHost();
 
     /**
+     * @param string $host
+     */
+    public function setHost($host);
+
+    /**
      * @return array|string
      */
     public function getSchemes();
+
+    /**
+     * @param array|string $schemes
+     */
+    public function setSchemes($schemes);
 
     /**
      * @return array|string
