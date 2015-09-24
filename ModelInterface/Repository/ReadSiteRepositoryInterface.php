5648473cadbd592a0c8d2047db36e724bed3c988
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-09-24 09:55:36 +0200

    change siteRepositoryInterface to ReadSiteRepositoryInterface

---
+++ ModelInterface/Repository/ReadSiteRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alavieille
+ * Date: 24/09/15
+ * Time: 09:34
+ */
+
+namespace OpenOrchestra\ModelInterface\Repository;
+
+
+interface ReadSiteRepositoryInterface
+{
+
+}
