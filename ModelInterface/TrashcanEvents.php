a533c9be6c0958f802cfdd474e71f45511bced8c
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-03-21 11:31:16 +0100

    add trashcan event

---
+++ ModelInterface/TrashcanEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OpenOrchestra\ModelInterface;
+
+/**
+ * Class TrashcanEvents
+ */
+final class TrashcanEvents
+{
+    const TRASHCAN_DELETE_ENTITY = 'trashcan.delete.entity';
+}
