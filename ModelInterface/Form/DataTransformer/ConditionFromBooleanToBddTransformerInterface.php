3a9e78e37ff08fe4ebebd5eb1ec6403dc4639168
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-05-23 11:05:36 +0200

    add const

--- ModelInterface/Form/DataTransformer/ConditionFromBooleanToBddTransformerInterface.php
+++ ModelInterface/Form/DataTransformer/ConditionFromBooleanToBddTransformerInterface.php
@@ -9,6 +9,12 @@ use Symfony\Component\Form\DataTransformerInterface;
  */
 interface ConditionFromBooleanToBddTransformerInterface extends DataTransformerInterface
 {
+    const GET_BALANCED_BRACKETS = '/\( ([^\(\)]*) \)/';
+    const GET_AND_SUB_BOOLEAN = '/(NOT (?=.)){0,1}([^ \(\)]+)( AND (?=.)){0,1}/';
+    const GET_OR_SUB_BOOLEAN = '/(NOT (?=.)){0,1}([^ \(\)]+)( OR (?=.)){0,1}/';
+    const IS_AND_BOOLEAN = '/^((NOT (?=.)){0,1}[^ \(\)]+( AND (?=.)){0,1})+$/';
+    const IS_OR_BOOLEAN = '/^((NOT (?=.)){0,1}[^ \(\)]+( OR (?=.)){0,1})+$/';
+
     /**
      * @param string $field
      */
