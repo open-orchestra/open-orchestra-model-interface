bdcb51c40b3bf1ad60fa050a796f8da250be3963
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-05-25 10:25:46 +0200

    PSR

--- ModelInterface/Model/EmbedKeywordInterface.php
+++ ModelInterface/Model/EmbedKeywordInterface.php
@@ -2,10 +2,12 @@
 
 namespace OpenOrchestra\ModelInterface\Model;
 
-@trigger_error('The '.__NAMESPACE__.'\EmbedKeywordInterface class is deprecated since version 1.2.0 and will be removed in 1.2.1', E_USER_DEPRECATED);
+@trigger_error('The '.__NAMESPACE__.'\EmbedKeywordInterface class is deprecated since version 1.2.0 and will be removed in 1.3.0', E_USER_DEPRECATED);
 
 /**
  * Interface EmbedKeywordInterface
+ *
+ * @deprecated will be removed in 1.3.0
  */
 interface EmbedKeywordInterface
 {
