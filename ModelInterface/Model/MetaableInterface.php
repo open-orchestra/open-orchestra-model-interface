378e4b8bdc2ba61fa045b6caa35569506adc13f4
Ali mbecheze Oumar <benomar01@gmail.com>
2016-06-02 14:40:57 +0200

    add new function for multilanguage

--- ModelInterface/Model/MetaableInterface.php
+++ ModelInterface/Model/MetaableInterface.php
@@ -2,30 +2,67 @@
 
 namespace OpenOrchestra\ModelInterface\Model;
 
+use Doctrine\Common\Collections\Collection;
+use OpenOrchestra\ModelInterface\Model\TranslatedValueContainerInterface;
+
 /**
  * interface MetaableInterface
  */
-interface MetaableInterface
+interface MetaableInterface extends TranslatedValueContainerInterface
 {
     /**
-     * @param string $metaKeywords
+     * @param Collection $metaKeywords
      */
-    public function setMetaKeywords($metaKeywords);
+    public function setMetaKeywords(Collection $metaKeywords);
 
     /**
+     * @param TranslatedValueInterface $metaKeyword
+     */
+    public function addMetaKeyword(TranslatedValueInterface $metaKeyword);
+
+    /**
+     * @param TranslatedValueInterface $metaKeyword
+     */
+    public function removeMetaKeyword(TranslatedValueInterface $metaKeyword);
+
+    /**
+     * @param string $language
+     *
      * @return string
+     */
+    public function getMetaKeyword($language = 'en');
+
+    /**
+     * @return Collection
      */
     public function getMetaKeywords();
 
     /**
-     * @param string $metaDescription
+     * @param Collection $metaDescription
      */
-    public function setMetaDescription($metaDescription);
+    public function setMetaDescriptions(Collection $metaDescription);
 
     /**
+     * @param TranslatedValueInterface $metaDescription
+     */
+    public function addMetaDescription(TranslatedValueInterface $metaDescription);
+
+    /**
+     * @param TranslatedValueInterface $metaDescription
+     */
+    public function removeMetaDescription(TranslatedValueInterface $metaDescription);
+
+    /**
+     * @param string $language
+     *
      * @return string
      */
-    public function getMetaDescription();
+    public function getMetaDescription($language = 'en');
+
+    /**
+     * @return Collection
+     */
+    public function getMetaDescriptions();
 
     /**
      * @param boolean $metaIndex
