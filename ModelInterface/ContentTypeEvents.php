7af16a3ee4bd2a81a5f2eaf81ecab225dcb9920c
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-08-17 17:41:44 +0200

    add events pre persits content type

--- ModelInterface/ContentTypeEvents.php
+++ ModelInterface/ContentTypeEvents.php
@@ -10,4 +10,5 @@ class ContentTypeEvents
     const CONTENT_TYPE_CREATE = 'contentType.create';
     const CONTENT_TYPE_DELETE = 'contentType.delete';
     const CONTENT_TYPE_UPDATE = 'contentType.update';
+    const CONTENT_TYPE_PRE_PERSIST = 'contentType.prePersist';
 }
