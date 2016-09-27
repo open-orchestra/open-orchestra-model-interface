ba9c732f0caa48540054b70ad5afe500c4451581
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-09-27 17:29:24 +0200

    update deprecated annotation (#231)

--- ModelInterface/Model/EmbedKeywordInterface.php
+++ ModelInterface/Model/EmbedKeywordInterface.php
@@ -2,12 +2,12 @@
 
 namespace OpenOrchestra\ModelInterface\Model;
 
-@trigger_error('The '.__NAMESPACE__.'\EmbedKeywordInterface class is deprecated since version 1.2.0 and will be removed in 1.3.0', E_USER_DEPRECATED);
+@trigger_error('The '.__NAMESPACE__.'\EmbedKeywordInterface class is deprecated since version 1.2.0 and will be removed in 2.0', E_USER_DEPRECATED);
 
 /**
  * Interface EmbedKeywordInterface
  *
- * @deprecated will be removed in 1.3.0
+ * @deprecated will be removed in 2.0
  */
 interface EmbedKeywordInterface
 {
--- ModelInterface/Repository/ReadContentRepositoryInterface.php
+++ ModelInterface/Repository/ReadContentRepositoryInterface.php
@@ -25,7 +25,7 @@ interface ReadContentRepositoryInterface
      * @param string      $choiceType
      * @param string|null $keywords
      *
-     * @deprecated will be removed in 1.3.0, use findByContentTypeAndCondition
+     * @deprecated will be removed in 2.0, use findByContentTypeAndCondition
      *
      * @return array
      */
