00dcdfac60fe8a33690beb37ee30c6cd4e65a2af
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-09-27 17:13:40 +0200

    remove unused event

--- ModelInterface/ContentTypeEvents.php
+++ ModelInterface/ContentTypeEvents.php
@@ -10,5 +10,4 @@ class ContentTypeEvents
     const CONTENT_TYPE_CREATE = 'contentType.create';
     const CONTENT_TYPE_DELETE = 'contentType.delete';
     const CONTENT_TYPE_UPDATE = 'contentType.update';
-    const CONTENT_TYPE_PRE_PERSIST = 'contentType.prePersist';
 }
