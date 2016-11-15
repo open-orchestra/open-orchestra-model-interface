3dc2994db40217c0160aff00c5b84c9533b56025
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-11-15 10:08:41 +0100

    optimize Block Update

--- ModelInterface/Event/NodeEvent.php
+++ ModelInterface/Event/NodeEvent.php
@@ -1,35 +1,32 @@
 <?php
-
 namespace OpenOrchestra\ModelInterface\Event;
-
 use OpenOrchestra\ModelInterface\Model\NodeInterface;
 use OpenOrchestra\ModelInterface\Model\StatusableInterface;
 use OpenOrchestra\ModelInterface\Model\StatusInterface;
 use Symfony\Component\EventDispatcher\Event;
 use OpenOrchestra\ModelInterface\Event\EventTrait\EventStatusable;
 use OpenOrchestra\ModelInterface\Event\EventTrait\EventStatusableInterface;
-
+use OpenOrchestra\ModelInterface\Model\BlockInterface;
 /**
  * Class NodeEvent
  */
 class NodeEvent extends Event implements EventStatusableInterface
 {
     use EventStatusable;
-
     protected $node;
     protected $area;
     protected $block;
-
     /**
      * @param NodeInterface        $node
      * @param StatusInterface|null $previousStatus
+     * @param BlockInterface|null  $block
      */
-    public function __construct(NodeInterface $node, $previousStatus = null)
+    public function __construct(NodeInterface $node, $previousStatus = null, $block = null)
     {
         $this->node = $node;
         $this->previousStatus = $previousStatus;
+        $this->block = $block;
     }
-
     /**
      * @return NodeInterface
      */
@@ -37,12 +34,18 @@ class NodeEvent extends Event implements EventStatusableInterface
     {
         return $this->node;
     }
-
     /**
      * @return StatusableInterface
      */
     public function getStatusableElement()
     {
         return $this->getNode();
+    }
+    /**
+     * @return BlockInterface
+     */
+    public function getBlock()
+    {
+        return $this->block;
     }
 }
