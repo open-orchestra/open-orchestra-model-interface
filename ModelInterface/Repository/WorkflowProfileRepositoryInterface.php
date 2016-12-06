0e1c9f376c7262aa93d8b02e693cb5dd6580a5f4
itkg-ngilain <noel.gilain@businessdecision.com>
2016-12-06 17:26:57 +0100

    Add WorkflowProfileRepository::hasTransition

--- ModelInterface/Repository/WorkflowProfileRepositoryInterface.php
+++ ModelInterface/Repository/WorkflowProfileRepositoryInterface.php
@@ -2,9 +2,19 @@
 
 namespace OpenOrchestra\ModelInterface\Repository;
 
+use OpenOrchestra\ModelInterface\Model\WorkflowTransitionInterface;
+
 /**
  * Interface WorkflowProfileRepositoryInterface
  */
 interface WorkflowProfileRepositoryInterface
 {
+    /**
+     * Test is $transition exists
+     *
+     * @param  WorkflowTransitionInterface $transition
+     *
+     * @return boolean
+     */
+    public function hasTransition(WorkflowTransitionInterface $transition);
 }
