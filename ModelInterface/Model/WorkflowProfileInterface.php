45d768b5c23ec43a1377feb2aea335f6ac2a9e34
itkg-ngilain <noel.gilain@businessdecision.com>
2016-12-06 11:36:00 +0100

    Add getters to workflow profile

--- ModelInterface/Model/WorkflowProfileInterface.php
+++ ModelInterface/Model/WorkflowProfileInterface.php
@@ -11,4 +11,11 @@ interface WorkflowProfileInterface
      * @param WorkflowTransitionInterface $transition
      */
     public function addTransition(WorkflowTransitionInterface $transition);
+
+    /**
+     * @param WorkflowTransitionInterface $transition
+     *
+     * @return boolean
+     */
+    public function hasTransition(WorkflowTransitionInterface $transition);
 }
