29ec17a8f9399054f4bef58ca5adb0097f655b44
Noël GILAIN <noel.gilain@businessdecision.com>
2017-01-30 16:54:12 +0100

    2 0 redirection (#282)
    * Update RedirectionRepository filter methods
    
    * Add RedirectionRepository::removeRedirections
    
    * Add RedirectionRepository::countByPattern
    
    * Update RedirectionRepository::countByPattern

--- ModelInterface/Repository/RedirectionRepositoryInterface.php
+++ ModelInterface/Repository/RedirectionRepositoryInterface.php
@@ -3,12 +3,12 @@
 namespace OpenOrchestra\ModelInterface\Repository;
 
 use OpenOrchestra\ModelInterface\Model\RedirectionInterface;
-use OpenOrchestra\Pagination\Configuration\PaginationRepositoryInterface;
+use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
 
 /**
  * Interface RedirectionRepositoryInterface
  */
-interface RedirectionRepositoryInterface extends PaginationRepositoryInterface
+interface RedirectionRepositoryInterface
 {
     /**
      * @return array
@@ -37,4 +37,38 @@ interface RedirectionRepositoryInterface extends PaginationRepositoryInterface
      * @return array
      */
     public function findBySiteId($siteId);
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return array
+     */
+    public function findForPaginate(PaginateFinderConfiguration $configuration);
+
+    /**
+     * @return int
+     */
+    public function count();
+
+    /**
+     * @param string $pattern
+     * @param string $redirectionId
+     *
+     * @return int
+     */
+    public function countByPattern($pattern, $redirectionId);
+
+    /**
+     * @param PaginateFinderConfiguration $configuration
+     *
+     * @return int
+     */
+    public function countWithFilter(PaginateFinderConfiguration $configuration);
+
+    /**
+     * @param array $redirectionIds
+     *
+     * @throws \Exception
+     */
+    public function removeRedirections(array $redirectionIds);
 }
