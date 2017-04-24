24e25a0bb2636acb4036cdc377b878b9743ba503
itkg-ngilain <noel.gilain@businessdecision.com>
2017-04-24 15:24:05 +0200

    Add NodeEvents::NODE_REMOVED

--- ModelInterface/NodeEvents.php
+++ ModelInterface/NodeEvents.php
@@ -7,16 +7,17 @@ namespace OpenOrchestra\ModelInterface;
  */
 final class NodeEvents
 {
-    const PATH_UPDATED = 'node.path_updated';
-    const NODE_UPDATE = 'node.update';
-    const NODE_UPDATE_BLOCK = 'node.update_block';
+    const NODE_UPDATE                = 'node.update';
+    const NODE_UPDATE_BLOCK          = 'node.update_block';
     const NODE_UPDATE_BLOCK_POSITION = 'node.update_block_position';
-    const NODE_CREATION = 'node.creation';
-    const NODE_DELETE = 'node.delete';
-    const NODE_DELETE_VERSION = 'node.delete.version';
-    const NODE_RESTORE = 'node.restore';
-    const NODE_DUPLICATE = 'node.duplicate';
-    const NODE_ADD_LANGUAGE = 'node.add_language';
-    const NODE_DELETE_BLOCK = 'node.delete_block';
-    const NODE_CHANGE_STATUS = 'node.status';
+    const NODE_CREATION              = 'node.creation';
+    const NODE_DELETE                = 'node.delete';
+    const NODE_DELETE_VERSION        = 'node.delete.version';
+    const NODE_RESTORE               = 'node.restore';
+    const NODE_DUPLICATE             = 'node.duplicate';
+    const NODE_ADD_LANGUAGE          = 'node.add_language';
+    const NODE_DELETE_BLOCK          = 'node.delete_block';
+    const NODE_CHANGE_STATUS         = 'node.status';
+    const PATH_UPDATED               = 'node.path.updated';
+    const NODE_REMOVED               = 'node.removed';
 }
